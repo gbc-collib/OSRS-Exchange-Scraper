953f4cbfc780e79bdbed7d18db279c1ac9da4869
Collin Stasiak <collinstasiak@gmail.com>
2022-09-22 17:16:23 -0400

    reduced runtime
    Cut back on redundant API requests cutting runtime in half especially for items searched by name.

--- osrs_site/get_price.py
+++ osrs_site/get_price.py
@@ -4,18 +4,18 @@ import requests #for handling url requests
 import json #converts string data into usable dictionary
 import datetime
 
+DATABASE = 'data_base.json'
+
 class ItemData:
 	def __init__(self, item_id): #item name accepts string of item name or iteger of item ID
 		self.item_id = item_id
 		self.item_name = None
+		self.high = None
+		self.low = None
+		self.icon = None
 	def name_to_id(self):
-		item_url = 'http://prices.runescape.wiki/api/v1/osrs/mapping'
-		headers = {
-			'User-Agent': 'gbc_collib\'s amateur price tracker',
-			'From': 'collinstasiak@gmail.com'
-		}
-		raw_data = requests.get(item_url, headers=headers, stream=True)
-		data = raw_data.json() # uses response's json handling to return dictoinary of item data
+		with open('data_base.json', 'r') as f:
+			data = json.load(f)
 		for item in data:
 			if ''.join(item['name'].lower().split()) == ''.join(self.item_id.lower().split()): #checks name to item id database and normalizes caps to make user friendly
 				self.item_name = item['name']
@@ -23,13 +23,13 @@ class ItemData:
 				return self.item_id
 		return False
 	def id_to_name(self):
-		url = 'https://secure.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item='
-		raw_data = requests.get(url + str(self.item_id))
-		if raw_data.status_code == 404:
-			self.item_name = 'Invalid'
-			return False
-		data = raw_data.json()
-		self.item_name = data['item']['name']
+		with open(DATABASE, 'r') as f:
+			data = json.load(f)
+		for item in data:
+			if item['id'] == ''.join(self.item_id.lower().split()): #checks name to item id database and normalizes caps to make user friendly
+				self.item_id = item['id']
+				self.name = str(item['name']) #return the id
+				return self.item_name
 
 	def grab_data(self): #to handle calling wiki and grabbing raw data to convert to usable format, returns a dictionary of item data if it's valid item, returns 0 if not
 		item_url = 'http://prices.runescape.wiki/api/v1/osrs/latest'
@@ -51,6 +51,9 @@ class ItemData:
 		 	'low': data['data'][self.item_id]['low'],
 		 	'icon': 'https://secure.runescape.com/m=itemdb_oldschool/1663239679537_obj_big.gif?id=' + str(self.item_id)
 		 	}
+			self.high = formatted_data['high']
+			self.low = formatted_data['low']
+			self.icon = formatted_data['icon']
 			return formatted_data
 		return False
 def main(args):
