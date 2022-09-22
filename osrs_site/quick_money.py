cb5b868f09b3ed77fda240875179f2862c7cf798
Collin Stasiak <collinstasiak@gmail.com>
2022-09-22 17:15:40 -0400

    Finished proof of concept for quick flips module

--- osrs_site/quick_money.py
+++ osrs_site/quick_money.py
@@ -4,6 +4,7 @@ with adamant or dragon bolts when to do so is profittable"""
 import requests
 import json
 import get_price
+import cProfile
 
 flip_list = 'get_rich.json' #a json file containing a graph of items with final product having vertices of ingridients
 
@@ -17,16 +18,35 @@ def print_tree(node, level=0):
         print(' ' * 4 * level + '-> ' + str(node.data))
         print_tree(node.right, level + 1)
 
-def calculate_profit():
+def update_flip_list():
     with open(flip_list, 'r') as f:
         potential_flips = json.load(f)
     profits = Node('flips')
     counter = 0
     for key in potential_flips:
-        profits.child.append(Node({key: get_price.ItemData(key).grab_data()['high']}))
+        item = get_price.ItemData(key).grab_data()
+        profits.child.append(Node(item))
         for ingredient in potential_flips[key]:
-            (profits.child[counter].child).append(Node({ingredient: get_price.ItemData(ingredient).grab_data()['low']}));
+            #(profits.child[counter].child).append(Node({ingredient: get_price.ItemData(ingredient).grab_data()['low']}));
+            item = get_price.ItemData(ingredient).grab_data()
+            profits.child[counter].child.append(item)
+            #profits.child[counter].child.append(Node(get_price.ItemData(ingredient).grab_data()['low']))
         counter += 1
+    return profits
+
+def calculate_profit():
+    flips = update_flip_list()
+    item_profit = {}
+    for item in flips.child:
+        profit = int(item.data['high'])
+        print(item.data['name'] + str(item.data['high']))
+        for ingredient in item.child:
+            print(ingredient['name'] + str(ingredient['low']))
+            profit -= int(ingredient['low'])
+#         - (int(item.child[0]['low']) + int(item.child[1]['low']))
+        item_profit[item.data['name']] = profit
+    print(item_profit)
+    return item_profit
 
 def main(args):
     calculate_profit()
@@ -35,12 +55,3 @@ def main(args):
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
-"""
-    formatted_data = {
-    'name': self.item_name,
-    'item_id': self.item_id,
-    'high': data['data'][self.item_id]['high'],
-    'low': data['data'][self.item_id]['low'],
-    'icon': 'https://secure.runescape.com/m=itemdb_oldschool/1663239679537_obj_big.gif?id=' + str(self.item_id)
-    }
-"""
