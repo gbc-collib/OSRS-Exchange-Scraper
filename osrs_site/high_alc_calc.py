eb8962e622af38564d350af721ae3fcbcf041a6d
Collin Stasiak <collinstasiak@gmail.com>
2022-10-05 16:36:22 -0400

    high_alc works and has table function

--- osrs_site/high_alc_calc.py
+++ osrs_site/high_alc_calc.py
@@ -1,35 +1,51 @@
 import json
 
 import get_price
-ITEM_DATABASE = 'items_osrs.json'
+
+ITEM_DATABASE = 'data_base.json'
+
 
 def grab_alc_price():
-	with open(ITEM_DATABASE) as f: #opens prebuilt item database and closes as soon as it's importeds
-		json_data = json.load(f)
-		itemslist = json_data.items()
+	with open(ITEM_DATABASE) as f:  # opens prebuilt item database and closes as soon as it's imported
+		items_list = json.load(f)
 		high_alc_values = {}
-		for item in itemslist:
-			high_alc_values[item[1]['name']] = item[1]['value']
+		for item in items_list:
+			if not 'highalch' in item or not 'limit' in item: #handles edge case of non-alch-able items or non tradable
+				continue
+			high_alc_values[item['name']] = item['highalch'], item['limit']
 	return high_alc_values
 
-def alc_profit(items=grab_alc_price(), margin=10): #takes dict keyed by item name with value of high alc, defaults to all items
-	nature_rune_cost = get_price.ItemData('561').grab_data()['low']
+def alc_profit(items: dict = grab_alc_price(), margin: int = 10, limit: int = 1000):  # takes dict keyed by item name with value of high alc, defaults to all items
+	api_object = get_price.ItemData('561')
+	nature_rune_cost = api_object.grab_data()['low']
+	print(nature_rune_cost)
 	profittable_alc = {}
 	for name, price in items.items():
-		ge_price = get_price.ItemData(name).grab_data()
-		if not ge_price:
+		api_object.item_id = name
+		if price[1] < limit or price[0] < nature_rune_cost:
 			continue
-		ge_price = ge_price['low']
-		actual_margin = ge_price - nature_rune_cost
-		if  actual_margin > margin:
-			profittable_alc[name] = actual_margin
+		ge_price = api_object.grab_data()
+		ge_price = ge_price['high']
+		if (price[0] - nature_rune_cost) < ge_price:
+			continue
+		potential_alc = (price[0] - nature_rune_cost) - ge_price
+		if potential_alc > margin:
+			profittable_alc[name] = potential_alc
 	return profittable_alc
+def sort_alc_list(list: dict):
+	return dict(sorted(list.items(), key=lambda item: item[1], reverse=True))
+
 
 def main(args):
-	print(alc_profit())
-	
+	alc_dict = alc_profit()
+	print("{:<10} {:<10}".format('Name', 'profit'))
+	for key, value in alc_dict.items():
+		print("{:<10} {:<10}".format(key, value))
+
 	return 0
+
 
 if __name__ == '__main__':
 	import sys
+
 	sys.exit(main(sys.argv))
